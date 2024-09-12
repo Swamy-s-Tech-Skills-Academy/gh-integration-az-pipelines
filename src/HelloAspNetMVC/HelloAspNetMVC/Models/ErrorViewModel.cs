942d2df36985b0bc65a578a5937055ddab5d3483
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-12 15:27:45 +0530

    Initial Version

---
+++ src/HelloAspNetMVC/HelloAspNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloAspNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
